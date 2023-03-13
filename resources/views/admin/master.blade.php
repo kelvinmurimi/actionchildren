fd77a4fb016085d283be5e40fb6a8791684e6ba5
kelvinmurimi <murimikelvin229@gmail.com>
2023-03-13 20:38:55 +0300

    minor chages

---
+++ resources/views/admin/master.blade.php
@@ -0,0 +1,17 @@
+
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+       <div class="container">
+        @yield('content')
+       </div>
+    </div>
+</x-app-layout> 
+
+
+
